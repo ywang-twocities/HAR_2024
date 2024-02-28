98c115f73076c1e458dbcd02e901e4ab58853fba
ywangpl <ywang.feb@outlook.com>
2024-02-28 09:33:32 +0000

    grid search for L, M added.

--- Method_Start_from_rise_time/Code_2024_HAR_rework/main.py
+++ Method_Start_from_rise_time/Code_2024_HAR_rework/main.py
@@ -12,15 +12,15 @@ import code
 def main():
     current_dir = Path(__file__)
     exp_envs = ["Complex_LOS-23-12-2022", "LOS-23-12-2022", "NLOS-22-12-2022"]
-    scenario = [0]
+    scenario = [1]
     # data_dir = current_dir.parents[2] / 'Dataset_12CIR' / exp_envs[scenario]
     config = load_config('config.json')
     number_cir_values = [12]
-    length_cir_values = [125]
+    length_cir_values = [150]
     model_names = ['knn']
     # number_cir_values = [8, 10, 12]
     # length_cir_values = [50, 75, 100, 125, 150, 175, 200, 225]
-    # model_names = ['gru', 'lstm', 'lstm_fcn', 'rnn', '2d_fcn', 'cnn', 'mlp', 'svm', 'knn', 'random forest',
+    # model_names = ['gru', 'lstm', 'lstm_fcn', 'rnn'50, , '2d_fcn', 'cnn', 'mlp', 'svm', 'knn', 'random forest',
     #                                                                          'naive bayesian']
     results = {}
 
@@ -45,11 +45,12 @@ def main():
                             f"Scenario: {exp_envs[sce]}, Model: {result_key[0]}, Number_CIR: {result_key[1]}, "
                             f"Length_CIR: {result_key[2]}, "
                             f"Accuracies: {accuracies}, Overall Accuracy: {overall_accuracy}%\n")
-
+                    return X, y
     end = time.time()
     print(f"{end - start}\n")
 
 
 if __name__ == '__main__':
-    main()
-
+    X, y = main()
+    import code
+    code.interact(local=dict(globals(), **locals()))
